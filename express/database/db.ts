8dd67862c98b81b9207c93d24cb644dfd824700e
Korvo2Tuirow <korvo2irow@live.com>
2025-05-16 21:02:03 -0300

    teste

--- express/database/db.ts
+++ express/database/db.ts
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore,  } from "firebase/firestore";
 
 const { API_KEY, AUTH_DOMAIN, PROJECT_ID, STORAGE_BUCKET, MESSAGING_SENDER_ID, APP_ID } = process.env;
 
